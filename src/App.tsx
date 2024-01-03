40a0326f435d2d84cae0fa9ff443b15e3a902221
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2024-01-04 03:45:46 +0900

    grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  height: '200px',
+  textAlign: 'center',
+  lineHeight: '200px',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box sx={{ flexGrow: 1, m: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Item>box</Item>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Item>box</Item>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Item>box</Item>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Item>box</Item>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
